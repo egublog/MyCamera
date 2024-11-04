ab0e01e992e21912660106071130f8c68795301a
egublog <egukei0729@gmail.com>
2024-11-04 23:59:53 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 江口敬太郎 on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
