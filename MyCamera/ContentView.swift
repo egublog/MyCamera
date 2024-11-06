ada26052449be714fad837d16bf9d9e586f82a10
egublog <egukei0729@gmail.com>
2024-11-07 00:00:15 +0900

    add: フォトライブラリーから選択する機能

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -6,13 +6,16 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     // 撮影した写真を保持する状態変数
     @State var captureImage: UIImage? = nil
     // 撮影画面(sheet)の開閉状態を管理
     @State var isShowSheet = false
-    
+    // フォトライブラリーで選択した写真を管理
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
+
     var body: some View {
         VStack {
             // スペース追加
@@ -26,7 +29,7 @@ struct ContentView: View {
                 // アスペクト比（縦横比）を維持して画面に収める
                     .scaledToFit()
             }
-            
+
             // スペース追加
             Spacer()
             // 「カメラを起動する」ボタン
@@ -62,7 +65,37 @@ struct ContentView: View {
                 // UIImagePickerController（写真撮影）を表示
                 ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
             } // 「カメラを起動する」ボタンのsheetここまで
-            
+
+            // フォトライブラリーから選択する
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                // テキスト表示
+                Text("フォトライブラリーから選択する")
+                // 横幅いっぱい
+                    .frame(maxWidth: .infinity)
+                // 高さ50ポイント指定
+                    .frame(height: 50)
+                // 背景を青色に指定
+                    .background(Color.blue)
+                // 文字色を白色に指定
+                    .foregroundStyle(Color.white)
+                // 上下左右に余白を追加
+                    .padding()
+            } // PhotosPickerここまで
+            // 選択した写真情報をもとに写真を取り出す
+            .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
+                // 選択した写真があるとき
+                if let newValue {
+                    Task {
+                        // Data型で写真を取り出す
+                        if let data = try? await newValue.loadTransferable(type: Data.self) {
+                            // 写真があるとき
+                            // 写真をcaptureImageに保存
+                            captureImage = UIImage(data: data)
+                        }
+                    }
+                }
+            }) // .onChange ここまで
+
             // captureImageをアンラップする
             if let captureImage {
                 // captureImageから共有する画像を生成する
